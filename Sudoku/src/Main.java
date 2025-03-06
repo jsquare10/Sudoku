f8b06d8d829ae797a72dedcd5c339a37ae766dcc
Dasaradhi Jetti <dasaradhi.jetti@mhk.com>
2025-03-05 20:35:39 -0700

    Sudoku Initial Commit

---
+++ Sudoku/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author jithu
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new SudokuUI().setVisible(true);
+    }
+}
